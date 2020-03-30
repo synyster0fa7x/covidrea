83366df85c255987debe9c72ed430796a29594ed
Anthony <anthony@ikuzo.fr>
2020-03-30 18:08:42 +0200

    Login page test

---
+++ tests/Browser/LoginPage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginPage extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->assertSee('Connexion')
+                ->assertPresent('button[type="submit"]')
+                ->assertPresent('#email')
+                ->assertPresent('#password')
+                ->assertSeeLink('Vous avez oublié votre mot de passe ?')
+            ;
+        });
+    }
+}
